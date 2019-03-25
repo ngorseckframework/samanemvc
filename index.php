4e8a1916f7b24c58aa68887e6a53cb9ecc6cb285
yengue <ibrahimayengue90@gmail.com>
2019-03-25 08:06:41 +0000

    add fastRouting

--- index.php
+++ index.php
@@ -9,8 +9,51 @@
     VOUS ETES LIBRE DE TOUTE UTILISATION.
   ===================================================*/
 
-	
+	/*
 	require_once 'libs/autoload/Autoloader.class.php';
   use libs\system\Bootstrap;
-  $bootstrap = new Bootstrap();
+  $bootstrap = new Bootstrap();*/
+
+  require "vendor/autoload.php";
+  $dispatcher = FastRoute\simpleDispatcher(
+    function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'get_all_users_handler');
+    // {id} must be a number (\d+)
+    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+  }
+);
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        echo 'ici';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        echo "ici2";
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        var_dump($handler);
+        var_dump($vars);
+        echo "ici3";
+        
+        break;
+}
 ?>
