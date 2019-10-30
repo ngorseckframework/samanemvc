bbbe0dc99b5b8b4f859f3abcaa56d1125c452d52
  ’Samane <samanemvc@gmail.com>
2019-10-30 17:22:26 +0000

    update Emailcontroller public folder

---
+++ cache/cec99d7014a789eb7ceb6e771660067bc0f1049d_0.file.liste.html.php
@@ -0,0 +1,127 @@
+<?php
+/* Smarty version 3.1.30, created on 2019-10-30 18:18:11
+  from "/Applications/XAMPP/xamppfiles/htdocs/mesprojets/Samane_workspace/samane1.9.0-Final/samanemvc/src/view/test/liste.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5db9c5d39603d6_95819982',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'cec99d7014a789eb7ceb6e771660067bc0f1049d' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/mesprojets/Samane_workspace/samane1.9.0-Final/samanemvc/src/view/test/liste.html',
+      1 => 1571921216,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5db9c5d39603d6_95819982 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>List</title>
+		<!-- l'appel de <?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+ vous permet de recupérer le chemin de votre site web  -->
+		<link type="text/css" rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+public/css/bootstrap.min.css"/>
+		<link type="text/css" rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+public/css/samane.css"/>
+		<style>
+			h1{ 
+				color: #40007d;
+			}
+		</style>
+	</head>
+	<body>
+		<img src="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+public/image/logo.jpg" class="resize" />
+		<div class="nav navbar navbar-default navbar-fixed-top">
+			<ul class="nav navbar-nav">
+				<!-- l'appel de <?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+ vous permet de recupérer le chemin de votre site web  -->
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+">Accueil</a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/index">Menu test page </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/getId/1">Menu test get id page </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/liste">Menu test list page </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Upload/index">upload file </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+PdfGenerator/generate">Samane Generate pdf file </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+ExcelGenerator/generate">Samane Generate excel file </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+ExcelGenerator/read">Samane read excel file </a></li>
+				<li><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Email/sendMail">Samane Mailing </a></li>
+			</ul>
+		</div>
+		<div class="col-md-8 col-xs-12 col-md-offset-2" style="margin-top:150px;">
+			<div class="panel panel-info">
+				<div class="panel-heading">BIENVENUE A VOTRE MODELE MVC</div>
+				<div class="panel-body">
+					<div class="alert alert-success" style="font-size:18px; text-align:justify;">
+						Merci, l'équipe samanemvc vous remercie :) : 
+						je vous ai préparé un CRUD qui marche. Lisez la documentation.
+						<br/>Et surtout noubliez pas de configurer votre base de données : ou? Dans le dossier config
+						puis éditez le fichier database.php. Mettez à on l'etat de la base! Bon code!!!!  :)
+					</div>
+					<?php if (isset($_smarty_tpl->tpl_vars['tests']->value)) {?>
+						<?php if ($_smarty_tpl->tpl_vars['tests']->value != null) {?>
+							<table class="table table-bordered table-stripped">
+								<tr>
+									<th>Identifiant</th>
+									<th>Valeur 1</th>
+									<th>Valeur 2</th>
+									<th>Action</th>
+									<th>Action</th>
+								</tr>
+								<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['tests']->value, 'test');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['test']->value) {
+?>
+									<tr>
+										<td><?php echo $_smarty_tpl->tpl_vars['test']->value->getId();?>
+</td>
+										<td><?php echo $_smarty_tpl->tpl_vars['test']->value->getValeur1();?>
+</td>
+										<td><?php echo $_smarty_tpl->tpl_vars['test']->value->getValeur2();?>
+</td>
+										<td><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/delete/<?php echo $_smarty_tpl->tpl_vars['test']->value->getId();?>
+">Supprimer</a></td>
+										<td><a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/edit/<?php echo $_smarty_tpl->tpl_vars['test']->value->getId();?>
+">Editer</a></td>
+									</tr>
+								<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+							</table>
+						<?php } else { ?>
+							Liste vide
+						<?php }?>
+					<?php }?>
+				</div>
+				<a href="<?php echo $_smarty_tpl->tpl_vars['url_base']->value;?>
+Test/add">Ajout d'un test</a>
+			</div>
+		</div>
+		
+	</body>
+</html>
+<?php }
+}
