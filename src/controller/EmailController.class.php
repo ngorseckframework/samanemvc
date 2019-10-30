e3ca72ec02d47f438186d9583e0521b633f8d099
  ’Samane <samanemvc@gmail.com>
2019-10-30 17:15:02 +0000

    update Emailcontroller

--- src/controller/EmailController.class.php
+++ src/controller/EmailController.class.php
@@ -39,49 +39,5 @@ class EmailController extends Controller
         return $this->view->load("email/index", $data);
 
     }
-    /*
-    public function sendMail()
-    {
-        $mail = new SamaneMailing();
-
-        
-        $from = 'ngorseckjee@gmail.com';
-        $companyName = 'SAMANE FRAMEWORK';
-        $recipients = array('ngorsecka@gmail.com');
-        $subject = 'Samane Information';
-        $body = 'Samane vous remercie de votre engagement. <b>Samane PHP Framework</b>';
-        $replyTo = null;
-        $cc = null;
-        $bcc = null;
-        
-        $attachments = array('public/folder/pdf/samane.pdf');
-            
-        $result = $mail->sendMail($from, $companyName, $recipients, $replyTo, $cc, $bcc, $attachments , $subject, $body);
-        
-        if($result)
-        {
-            $mail = 'ok';
-        } else {
-            $mail = 'nok';
-        }
-        
-        return $this->view->redirect("Login/send?message=$mail");
-
-    }
-    public function send()
-    {
-        if(isset($_GET['message']))
-        {
-            if($_GET['message'] == 'ok')
-            {
-                $data['emailResult'] = 'Message sent successfully';
-            } else {
-                $data['emailResult'] = 'Message could not be sent';
-            }
-        }
-        return $this->view->load("email/index", $data);
-
-    }
-    */
 }
 ?>
